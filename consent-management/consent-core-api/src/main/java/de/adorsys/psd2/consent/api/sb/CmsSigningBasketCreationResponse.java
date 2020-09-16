4f2aca8c98cd6c958407a2a1d6616cfa5efc1d73
kyaex <kya@adorsys.com.ua>
2020-09-16 11:23:14 +0300

    extracted sb

--- consent-management/consent-core-api/src/main/java/de/adorsys/psd2/consent/api/sb/CmsSigningBasketCreationResponse.java
+++
@@ -1,9 +0,0 @@
-package de.adorsys.psd2.consent.api.sb;
-
-import lombok.Value;
-
-@Value
-public class CmsSigningBasketCreationResponse {
-    private String basketId;
-    private CmsSigningBasket cmsSigningBasket;
-}
